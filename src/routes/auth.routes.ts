72746fc98caeb7390f9edb1598f311d462ba9200
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-04-04 05:44:31 +0100

    add auth route

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { AuthController } from "../controllers/auth.controller";
+import Validator from "../validator";
+import authValidator from "../validator/auth.validator";
+
+const user: any = Validator.method;
+
+class AuthRoute {
+  public router: Router;
+  constructor() {
+    this.router = Router();
+    this.routes();
+  }
+  routes = () => {
+    const prefix: string = "auth";
+    this.router.post(
+      `${prefix}/create`,
+      authValidator.validate(user.createUser),
+      AuthController.register
+    );
+    this.router.post(
+      `${prefix}/login`,
+      authValidator.validate(user.login),
+      AuthController.login
+    );
+  };
+}
+
+export { AuthRoute };
