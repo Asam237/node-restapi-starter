368e767291ed31fe8188fbb2996ce7ce904408a4
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-04-04 04:59:29 +0100

    add login controller and README

---
+++ README.md
@@ -0,0 +1,42 @@
+![Screenshot from 2023-04-01 09-33-04](https://user-images.githubusercontent.com/34966088/229275452-385effeb-c076-4621-a057-a645a8870e5b.png)
+![Screenshot from 2023-04-01 08-34-10](https://user-images.githubusercontent.com/34966088/229272506-4fa50e49-1c77-404e-823d-e446729e7b88.png)
+![Screenshot from 2023-04-01 08-34-20](https://user-images.githubusercontent.com/34966088/229272507-97430f5c-48f8-4085-b592-6cf1fab2e9d4.png)
+
+## Introduction
+
+Clone of Spotify application
+<br/>
+
+## Composition
+
+This starter kit is composed of:
+
+- [Node.js](https://nodejs.org)
+  <br/>
+- [Express.js](https://expressjs.com)
+  <br/>
+- [JsonWebToken](https://www.npmjs.com/package/jsonwebtoken)
+  <br/>
+- [Validator](https://www.npmjs.com/package/validator)
+  <br/>
+- [Bcryptjs](https://www.npmjs.com/package/bcryptjs)
+  <br/>
+
+## Development
+
+## Deployment
+
+## Authors
+
+This project is maintained by:
+
+- Abba Sali Aboubakar Mamate ([@asam_237](https://twitter.com/asam_237))
+- my website: [https://abbasali.vercel.app](https://abbasali.vercel.app)
+- email: abbasalimokolo@gmail.com
+- mobile: (+237) 691846922
+
+<br/>
+
+## License
+
+[The MIT License.](https://opensource.org/licenses/MIT)
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,21 +1,48 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 import * as bcrypt from "bcryptjs";
-import {UserModel} from "../models/user.model";
+import * as jwt from "jsonwebtoken";
+import { UserModel } from "../models/user.model";
+import { TokenInfo } from "../core/types";
+import { EXPIRES, JWT_SECRET } from "../core/config";
 
 class AuthController {
-  public static async register(req: Request, res: Response, next: NextFunction): Promise<any> {
+  public static async register(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
     try {
-      const {firsname, lastname, email}: any = req.body
+      const { firsname, lastname, email }: any = req.body;
       const password: string = bcrypt.hashSync(req.body.password, 10);
-      const userParams: any = {firsname, lastname, email, password}
+      const userParams: any = { firsname, lastname, email, password };
       const user: any = new UserModel(userParams);
-      await user.save()
-      return res.json({user});
+      await user.save();
+      return res.json({ user });
+    } catch (error) {
+      console.log(error);
     }
-    catch(error) {
-      console.log(error)
+  }
+  public static async login(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
+    const { email, password }: any = req.body;
+    try {
+      const user: any = await UserModel.findOne({ email });
+      if (!user) return res.json({ message: "Login failed !" });
+      const isMatch: boolean = bcrypt.compareSync(password, user.password);
+      if (!isMatch) return res.json({ message: "Login failed !" });
+      const { _id } = user;
+      const tokenInfo: TokenInfo = { id: _id };
+      const token: string = jwt.sign(tokenInfo, JWT_SECRET, {
+        expiresIn: EXPIRES,
+      });
+      return res.json({ user, token });
+    } catch (error) {
+      console.log(error);
     }
   }
 }
 
-export {AuthController}
+export { AuthController };
