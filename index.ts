2ba42fb7f846e5bc70f0846f0842fb35261b171c
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-04-05 05:05:56 +0100

    feat: add starter

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
 import express from "express";
+import { Routes } from "./src/routes";
+import * as http from "http";
+import { PORT } from "./src/core/config";
+import { connectToDb } from "./src/core/connect/db";
 
 const app: express = express();
+Routes.init(app);
+const server: http.Server = http.createServer(app);
+
+const main = () => {
+  try {
+    server.listen(PORT, async () => {
+      await connectToDb();
+      console.log(`[Server]: running on ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
