48bd116c2db82bcd7c0d7e5f2585d9596296cc4b
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-06-03 21:52:26 +0100

    add others routes

--- src/routes/task.route.ts
+++ src/routes/task.route.ts
@@ -5,6 +5,9 @@ const TaskRoute = () => {
   const router = Router();
   const prefix: string = "/tasks";
   router.post(`${prefix}/create`, taskController.createTaskController);
+  router.get(`${prefix}/:id`, taskController.oneTaskController);
+  router.get(`${prefix}`, taskController.allTaskController);
+  router.delete(`${prefix}/:id`, taskController.destroyTaskController);
   return router;
 };
 
