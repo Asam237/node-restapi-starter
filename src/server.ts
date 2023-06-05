b451c901d88865d74b2c79962b4e075ec6897d5b
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-06-05 07:05:24 +0100

    feat: update fields of swagger

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import { TaskRoute } from "./routes/task.route";
 import { UserRoute } from "./routes/user.route";
 import * as swaggerUi from "swagger-ui-express"
 import * as swaggerDoc from "./swagger.json"
+import cors from "cors"
 
 export const setupRestEndpoints = (app: Application) => {
   const router = express.Router();
   app.use(express.json());
+  app.use(cors())
   app.use("/", router);
   app.use("/", UserRoute());
   app.use("/", TaskRoute());
